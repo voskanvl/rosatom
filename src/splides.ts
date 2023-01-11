fd9c76310a44fc3852d76abab6a996a4f3b13942
voskanvl <voskanvl@gmail.com>
2023-01-11 14:29:35 +0300

    8(team)

--- src/splides.ts
+++ src/splides.ts
@@ -3,6 +3,7 @@ import { MSplides } from "./initSlides";
 export default function splides() {
     const splidesInstance = new MSplides();
     const innopolis = document.querySelector("#innopolis");
+    const team = document.querySelector("#team");
 
     innopolis &&
         splidesInstance.add("#innopolis", {
@@ -25,4 +26,24 @@ export default function splides() {
         (innopolisSplideControlLeft.onclick = () => innopolisSplideInstance.go("<"));
     !!innopolisSplideControlRight &&
         (innopolisSplideControlRight.onclick = () => innopolisSplideInstance.go(">"));
+
+    team &&
+        splidesInstance.add("#team", {
+            type: "loop",
+            arrows: false,
+            pagination: false,
+            perMove: 1,
+            perPage: 4,
+            padding: "10px",
+            // focus: "center",
+        });
+    const teamSplideInstance = splidesInstance.instances["#team"];
+    const teamSplideControlLeft = document.querySelector<HTMLButtonElement>(
+        ".team__control-button--left",
+    );
+    const teamSplideControlRight = document.querySelector<HTMLButtonElement>(
+        ".team__control-button--right",
+    );
+    !!teamSplideControlLeft && (teamSplideControlLeft.onclick = () => teamSplideInstance.go("<"));
+    !!teamSplideControlRight && (teamSplideControlRight.onclick = () => teamSplideInstance.go(">"));
 }
