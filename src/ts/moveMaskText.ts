39ab2bb9617f1605905c46f9f1f2c5b0c484f69a
voskanvl <voskanvl@gmail.com>
2023-03-06 12:08:57 +0300

    minor changes

--- src/ts/moveMaskText.ts
+++ src/ts/moveMaskText.ts
@@ -2,7 +2,7 @@ export default function moveMaskText(element: HTMLElement) {
     element.addEventListener("mousemove", (event: MouseEvent) => {
         const { width } = getComputedStyle(element);
         const widthValue = parseInt(width);
-        element.style.setProperty("--x", widthValue - event.offsetX + "px");
+        element.style.setProperty("--x", widthValue - event.offsetX + 50 + "px");
     });
     element.addEventListener("mouseout", () => {
         element.style.setProperty("--x", "");
