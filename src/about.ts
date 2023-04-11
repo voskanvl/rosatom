f0c4e2f948b71eecf122e3d07afff851543916fc
voskanvl <voskanvl@gmail.com>
2023-04-11 17:29:22 +0300

    about page start

---
+++ src/about.ts
@@ -0,0 +1,29 @@
+import "./sass/style.sass";
+import "@splidejs/splide/css";
+import cursor from "./ts/components/cursor/cursor";
+import clickToLogo from "./ts/components/main-header/clickToLogo";
+import hideBurgerThenMenu from "./ts/components/burger/hideBurgerThenMenu";
+import burger from "./ts/components/burger";
+import closeMenu from "./ts/components/main-header/closeMenu";
+import toggleMainMenu from "./ts/components/main-header/toggleMainMenu";
+import toggleMobileMenu from "./ts/mobile/toggleMobileMenu";
+import applyMaskText from "./ts/applyMaskText";
+import { controlSearchPopup } from "./ts/components/popup/changeSearchPopup";
+
+cursor();
+clickToLogo();
+burger.open();
+burger.change();
+burger.close();
+hideBurgerThenMenu();
+closeMenu("ul.list");
+closeMenu(".burger__list");
+
+toggleMainMenu();
+toggleMobileMenu();
+applyMaskText();
+
+controlSearchPopup({
+    close: ".search-popup__close",
+    open: ".options__search",
+});
