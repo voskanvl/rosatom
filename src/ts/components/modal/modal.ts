6b5787388918acbebac1a8048d624e2c91c71f97
voskanvl <voskanvl@gmail.com>
2023-04-20 15:16:58 +0300

    modal

---
+++ src/ts/components/modal/modal.ts
@@ -0,0 +1,28 @@
+type ModalOptionType = {
+    blockScroll: boolean;
+};
+
+export default function modal(
+    el: HTMLElement,
+    { blockScroll }: ModalOptionType = { blockScroll: true },
+) {
+    if (blockScroll) {
+        document.body.style.height = "100vh";
+        document.body.style.overflowY = "hidden";
+    }
+    const modalEl = document.createElement("div");
+    const closeEl = document.createElement("div");
+
+    modalEl.classList.add("modal");
+    closeEl.classList.add("modal__close");
+
+    el.append(closeEl);
+    modalEl.append(el);
+    document.body.append(modalEl);
+
+    closeEl.addEventListener("click", () => {
+        document.body.style.height = "";
+        document.body.style.overflowY = "";
+        modalEl.remove();
+    });
+}
