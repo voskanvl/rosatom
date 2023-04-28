c1d20c88a5f67c3de09bf8fe2a7a6a5fd402a7d7
voskanvl <voskanvl@gmail.com>
2023-04-28 11:52:45 +0300

    change about banner

--- src/common.ts
+++ src/common.ts
@@ -2,11 +2,9 @@ import "./sass/style.sass";
 import "@splidejs/splide/css";
 
 import initHeader from "./ts/initHeader";
-import { setBlackCursor } from "./ts/components/cursor/cursor";
 import IMask from "imask";
 
 initHeader();
-setBlackCursor();
 
 const phones = document.querySelectorAll<HTMLInputElement>("input[name='phone']");
 phones &&
