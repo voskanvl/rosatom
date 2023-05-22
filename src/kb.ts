d46a4d5896f51c79d1e2b92ecc04318ac7900419
voskanvl <voskanvl@gmail.com>
2023-05-22 18:11:15 +0300

    kb aside

--- src/kb.ts
+++ src/kb.ts
@@ -15,3 +15,36 @@ aside &&
             ? catalogList && catalogList.removeAttribute("open")
             : catalogList && catalogList.setAttribute("open", "open")
     })
+
+//--- mobile aside ---
+const mobileAsideTrigger = document.querySelector<HTMLElement>(".kb__open-mobile-aside")
+mobileAsideTrigger &&
+    mobileAsideTrigger.addEventListener("click", () => {
+        const aside = mobileAsideTrigger.nextElementSibling as HTMLElement | null
+        if (!aside) return
+        const open = aside.getAttribute("open")
+        if (open) {
+            aside.removeAttribute("open")
+            mobileAsideTrigger.removeAttribute("open")
+        } else {
+            aside.setAttribute("open", "open")
+            mobileAsideTrigger.setAttribute("open", "open")
+        }
+    })
+
+document.addEventListener("click", (event: Event) => {
+    const targetEl = event.target as HTMLElement
+
+    const aside = document.querySelector<HTMLElement>(".kb__aside")
+    if (!aside) return
+
+    const inOfAside = targetEl.closest<HTMLElement>(".kb__aside")
+    const inOfMobileAsideTrigger = targetEl.closest<HTMLElement>(".kb__open-mobile-aside")
+
+    const open = aside.getAttribute("open")
+
+    if (open && !inOfAside && !inOfMobileAsideTrigger) {
+        aside && aside.removeAttribute("open")
+        mobileAsideTrigger && mobileAsideTrigger.removeAttribute("open")
+    }
+})
