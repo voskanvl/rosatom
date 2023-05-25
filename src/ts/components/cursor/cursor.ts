863933e24728b6021dd0cc20e209ccfc0f1e7913
voskanvl <voskanvl@gmail.com>
2023-05-25 10:00:17 +0300

    min fixes

--- src/ts/components/cursor/cursor.ts
+++ src/ts/components/cursor/cursor.ts
@@ -1,62 +1,62 @@
-import { SCREEN_NUMBER_TO_CHANGE } from "../../config";
-import store from "../../store";
+import { SCREEN_NUMBER_TO_CHANGE } from "../../config"
+import store from "../../store"
 
 export const setBlackCursor = () => {
-    document.body.style.cursor = "url('../../assets/cursor/mini-black.svg') 10 10, none";
-    const curR = document.querySelector<HTMLElement>(".cursor__round");
-    curR && (curR.style.borderColor = "#303031");
-};
+    document.body.style.cursor = "url('../../assets/cursor/mini-black.svg') 10 10, none"
+    const curR = document.querySelector<HTMLElement>(".cursor__round")
+    curR && (curR.style.borderColor = "#303031")
+}
 
 export const handlerStore = (cursorRound: HTMLElement, x: boolean) => {
     x
         ? (function () {
-              cursorRound.style.setProperty("--color", "#fff");
-              document.body.style.cursor = "url('../../assets/cursor/mini-white.svg') 10 10, none";
+              cursorRound.style.setProperty("--color", "#fff")
+              document.body.style.cursor = "url('../../assets/cursor/mini-white.svg') 10 10, none"
           })()
         : (function () {
-              cursorRound.style.setProperty("--color", "#303031");
+              cursorRound.style.setProperty("--color", "#303031")
               document.body.style.cursor = SCREEN_NUMBER_TO_CHANGE.some(
                   e => e === store.store.getState().activeScreenNumber,
               )
                   ? "url('../../assets/cursor/mini-white.svg') 10 10, none"
-                  : "url('../../assets/cursor/mini-black.svg') 10 10, none";
-          })();
-};
+                  : "url('../../assets/cursor/mini-black.svg') 10 10, none"
+          })()
+}
 
 export default function cursor() {
     // initCursor();
 
-    const cursorRound = document.querySelector<HTMLElement>(".cursor__round");
-    if (!cursorRound) throw Error("there isn't .cursor__round");
+    const cursorRound = document.querySelector<HTMLElement>(".cursor__round")
+    if (!cursorRound) throw Error("there isn't .cursor__round")
 
     document.addEventListener("mousemove", (event: MouseEvent) => {
-        const { clientX, clientY } = event;
-        cursorRound.style.setProperty("--x", clientX + "px");
-        cursorRound.style.setProperty("--y", clientY + "px");
-    });
+        const { clientX, clientY } = event
+        cursorRound.style.setProperty("--x", clientX + "px")
+        cursorRound.style.setProperty("--y", clientY + "px")
+    })
 
     document.addEventListener("mousedown", () => {
-        cursorRound.style.scale = "0.8 0.8";
-        cursorRound.style.translate = "-1px -1px";
-    });
+        cursorRound.style.scale = "0.85 0.85"
+        cursorRound.style.translate = "-3px -3px"
+    })
     document.addEventListener("mouseup", () => {
-        cursorRound.style.scale = "";
-        cursorRound.style.translate = "";
-    });
+        cursorRound.style.scale = ""
+        cursorRound.style.translate = ""
+    })
 
     //---   init cursor ---
-    handlerStore(cursorRound, true);
+    handlerStore(cursorRound, true)
 
     store.store.subscribe(({ activeScreenNumber }) => {
         handlerStore(
             cursorRound,
             SCREEN_NUMBER_TO_CHANGE.some(e => e === activeScreenNumber),
-        );
-    });
+        )
+    })
     store.menuStore.subscribe(({ isOpen }) => {
-        handlerStore(cursorRound, isOpen);
-    });
+        handlerStore(cursorRound, isOpen)
+    })
     store.burgerStore.subscribe(({ isOpen }) => {
-        handlerStore(cursorRound, isOpen);
-    });
+        handlerStore(cursorRound, isOpen)
+    })
 }
