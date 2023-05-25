a7e8f476c3b7bacc4ffb707e2f7f4886dfcf0104
voskanvl <voskanvl@gmail.com>
2023-05-25 20:49:08 +0300

    change fire show menu in main-head

--- src/ts/components/main-header/toggleMainMenu.ts
+++ src/ts/components/main-header/toggleMainMenu.ts
@@ -1,32 +1,32 @@
-import Store from "../../store";
+import Store from "../../store"
 
 export default function toggleMainMenu() {
-    const mainHeader = document.querySelector<HTMLElement>(".main-header");
-    const mainMenu = document.querySelector<HTMLElement>(".menu");
-    const triggers = [...document.querySelectorAll<HTMLElement>(".menu > .menu__item > span")];
+    const mainHeader = document.querySelector<HTMLElement>(".main-header")
+    const mainMenu = document.querySelector<HTMLElement>(".menu")
+    const triggers = [...document.querySelectorAll<HTMLElement>(".menu > .menu__item > span")]
 
     const matching: { trigger: HTMLElement; menu: HTMLElement }[] = triggers.map(e => ({
         trigger: e,
         menu: e.previousElementSibling as HTMLElement,
-    }));
+    }))
 
     matching.forEach(({ menu, trigger }) => {
-        trigger.addEventListener("click", () => {
+        trigger.addEventListener("mouseenter", () => {
             matching.forEach(
                 ({ menu: menuOthers }) =>
                     menu !== menuOthers && menuOthers.classList.remove("show"),
-            );
-            menu.classList.toggle("show");
+            )
+            menu.classList.toggle("show")
             if (matching.some(({ menu }) => menu.classList.contains("show"))) {
-                Store.menuStore.getState().open();
-                mainMenu && mainMenu.classList.add("show");
-                mainHeader && mainHeader.classList.add("show");
+                Store.menuStore.getState().open()
+                mainMenu && mainMenu.classList.add("show")
+                mainHeader && mainHeader.classList.add("show")
             } else {
-                Store.menuStore.getState().close();
-                mainMenu && mainMenu.classList.remove("show");
-                mainHeader && mainHeader.classList.remove("show");
-                mainHeader && (mainHeader.style.cursor = "inherit");
+                Store.menuStore.getState().close()
+                mainMenu && mainMenu.classList.remove("show")
+                mainHeader && mainHeader.classList.remove("show")
+                mainHeader && (mainHeader.style.cursor = "inherit")
             }
-        });
-    });
+        })
+    })
 }
