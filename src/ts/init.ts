17876e6ae66af06e0cc809324c76107971545384
voskanvl <voskanvl@gmail.com>
2023-05-26 10:02:50 +0300

    map pre fixed

--- src/ts/init.ts
+++ src/ts/init.ts
@@ -1,99 +1,99 @@
-import { scrollScreens } from "./scrollScreens";
-import toggleLogoImage from "./components/main-header/toggleLogoImage";
-import { controlSearchPopup } from "./components/popup/changeSearchPopup";
+import { scrollScreens } from "./scrollScreens"
+import toggleLogoImage from "./components/main-header/toggleLogoImage"
+import { controlSearchPopup } from "./components/popup/changeSearchPopup"
 // import changeMainHeaderByScroll from "./changeMainHeaderByScroll";
-import screenSwitcher from "./components/screen-swithcer/screenSwitcher";
-import advantagesHovers from "./advantagesHovers";
-import allocateRegionIcons from "./components/regions/allocateRegionIcons";
+import screenSwitcher from "./components/screen-swithcer/screenSwitcher"
+import advantagesHovers from "./advantagesHovers"
+import allocateRegionIcons from "./components/regions/allocateRegionIcons"
 
-import newsFeedHandler from "./news-feed-handeler";
-import splides from "./components/sliders/splides";
-import handlerRegionMove from "./components/regions/handlerRegionMove";
-import hideScreenSwitcherByMenu from "./components/screen-swithcer/hideScreenSwitcherByMenu";
-import touchedScroll from "./mobile/touchedScroll";
+import newsFeedHandler from "./news-feed-handeler"
+import splides from "./components/sliders/splides"
+import handlerRegionMove from "./components/regions/handlerRegionMove"
+import hideScreenSwitcherByMenu from "./components/screen-swithcer/hideScreenSwitcherByMenu"
+import touchedScroll from "./mobile/touchedScroll"
 
-import applyMaskText from "./applyMaskText";
-import applyIMaskToField from "./applyIMaskToField";
-import toggleMobileMenu from "./mobile/toggleMobileMenu";
-import toggleMainMenu from "./components/main-header/toggleMainMenu";
-import changeScreenSwitcherByScroll from "./components/screen-swithcer/changeScreenSwitcherByScroll";
+import applyMaskText from "./applyMaskText"
+import applyIMaskToField from "./applyIMaskToField"
+import toggleMobileMenu from "./mobile/toggleMobileMenu"
+import toggleMainMenu from "./components/main-header/toggleMainMenu"
+import changeScreenSwitcherByScroll from "./components/screen-swithcer/changeScreenSwitcherByScroll"
 // import changePointerByScreen from "./changePointerByScreen";
-import clickToLogo from "./components/main-header/clickToLogo";
-import closeMenu from "./components/main-header/closeMenu";
-import changeMainHeader from "./components/main-header/changeMainHeader";
-import burger from "./components/burger";
+import clickToLogo from "./components/main-header/clickToLogo"
+import closeMenu from "./components/main-header/closeMenu"
+import changeMainHeader from "./components/main-header/changeMainHeader"
+import burger from "./components/burger"
 // import changeCursorByMenu from "./changeCursorByMenu";
-import hideBurgerThenMenu from "./components/burger/hideBurgerThenMenu";
-import cursor from "./components/cursor/cursor";
-import MobileDetect from "mobile-detect";
-import randomizePulsIcons from "./components/regions/randomizePulsIcons";
-import store from "./store";
-import changeSearchPopupByScreensAndMenu from "./components/popup/changeSearchPopupByScreensAndMenu";
-import handleCODSlider from "./components/sliders/handleCODSlider";
+import hideBurgerThenMenu from "./components/burger/hideBurgerThenMenu"
+import cursor from "./components/cursor/cursor"
+import MobileDetect from "mobile-detect"
+import randomizePulsIcons from "./components/regions/randomizePulsIcons"
+import store from "./store"
+import changeSearchPopupByScreensAndMenu from "./components/popup/changeSearchPopupByScreensAndMenu"
+import handleCODSlider from "./components/sliders/handleCODSlider"
 
 export default function init() {
-    cursor();
+    cursor()
     // changePointerByScreen();
     // changeCursorByMenu();
-    clickToLogo();
+    clickToLogo()
 
-    burger.open();
-    burger.change();
-    burger.close();
-    hideBurgerThenMenu();
+    burger.open()
+    burger.change()
+    burger.close()
+    hideBurgerThenMenu()
 
-    toggleLogoImage();
-    scrollScreens();
-    hideScreenSwitcherByMenu();
+    toggleLogoImage()
+    scrollScreens()
+    hideScreenSwitcherByMenu()
 
-    closeMenu("ul.list");
-    closeMenu(".burger__list");
+    closeMenu("ul.list")
+    closeMenu(".burger__list")
 
-    toggleMainMenu();
-    toggleMobileMenu();
+    toggleMainMenu()
+    toggleMobileMenu()
 
-    touchedScroll();
+    touchedScroll()
 
-    applyMaskText();
+    applyMaskText()
 
     controlSearchPopup({
         close: ".search-popup__close",
         open: ".options__search",
-    });
-    changeSearchPopupByScreensAndMenu();
+    })
+    changeSearchPopupByScreensAndMenu()
 
     // changeMainHeaderByScroll();
-    changeMainHeader();
+    changeMainHeader()
 
-    changeScreenSwitcherByScroll();
-    screenSwitcher();
-    advantagesHovers();
+    changeScreenSwitcherByScroll()
+    screenSwitcher()
+    advantagesHovers()
 
-    splides();
+    splides()
 
-    newsFeedHandler();
-    handlerRegionMove();
+    newsFeedHandler()
+    handlerRegionMove()
 
-    applyIMaskToField();
+    applyIMaskToField()
 
-    allocateRegionIcons();
-    window.addEventListener("resize", () => allocateRegionIcons());
-    store.store.subscribe(({ activeScreenNumber }) => {
-        const { matches } = matchMedia("(max-width: 768px)");
-        const map = document.querySelector<HTMLElement>(".geonet__map");
-        if (activeScreenNumber === 4 && matches) {
-            !!map && (map.style.scale = "1.7 1.7");
-        } else {
-            !!map && (map.style.scale = "");
-        }
-    });
-    const md = new MobileDetect(window.navigator.userAgent);
+    allocateRegionIcons()
+    window.addEventListener("resize", () => allocateRegionIcons())
+    // store.store.subscribe(({ activeScreenNumber }) => {
+    //     const { matches } = matchMedia("(max-width: 768px)");
+    //     const map = document.querySelector<HTMLElement>(".geonet__map");
+    //     if (activeScreenNumber === 4 && matches) {
+    //         !!map && (map.style.scale = "1.7 1.7");
+    //     } else {
+    //         !!map && (map.style.scale = "");
+    //     }
+    // });
+    const md = new MobileDetect(window.navigator.userAgent)
     if (md.mobile()) {
-        const cursorRound = document.querySelector<HTMLElement>(".cursor__round");
-        !!cursorRound && (cursorRound.style.display = "none");
+        const cursorRound = document.querySelector<HTMLElement>(".cursor__round")
+        !!cursorRound && (cursorRound.style.display = "none")
     }
 
-    randomizePulsIcons();
+    randomizePulsIcons()
 
-    handleCODSlider();
+    handleCODSlider()
 }
