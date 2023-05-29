973e378b2ba563a65e0cfa9f7469d9c14743502e
voskanvl <voskanvl@gmail.com>
2023-05-29 09:43:30 +0300

    fix error output

--- src/common.ts
+++ src/common.ts
@@ -1,23 +1,23 @@
-import "./sass/style.sass";
-import "@splidejs/splide/css";
+import "./sass/style.sass"
+import "@splidejs/splide/css"
 
-import initHeader from "./ts/initHeader";
-import IMask from "imask";
-import { setBlackCursor } from "./ts/components/cursor/cursor";
+import initHeader from "./ts/initHeader"
+import IMask from "imask"
+import { setBlackCursor } from "./ts/components/cursor/cursor"
 
-initHeader();
-setBlackCursor();
+initHeader()
+setBlackCursor()
 
-const phones = document.querySelectorAll<HTMLInputElement>("input[name='phone']");
+const phones = document.querySelectorAll<HTMLInputElement>("input[name='phone']")
 phones &&
     phones.length &&
     phones.forEach(phone => {
         IMask(phone, {
             mask: "+{7}(000)000-00-00",
             lazy: false,
-        });
-    });
+        })
+    })
 
-const mainHeader = document.querySelector<HTMLElement>(".main-header");
-mainHeader && (mainHeader.style.position = "absolute");
-mainHeader && mainHeader.classList.remove("main-header--white");
+const mainHeader = document.querySelector<HTMLElement>(".main-header")
+mainHeader && (mainHeader.style.position = "absolute")
+mainHeader && mainHeader.classList.remove("main-header--white")
--- src/excursion.ts
+++ src/excursion.ts
@@ -1,5 +1,6 @@
 import { z, ZodError } from "zod"
 import { setWhiteCursor } from "./ts/components/cursor/cursor"
+import IMask from "imask"
 
 //--- CHECK ABOUT PAGE
 const isExcursionPage = !!document.querySelector("section.excursion")
@@ -28,6 +29,15 @@ const form = document.querySelector<HTMLFormElement>("form")
 const fieldsetUlEl = form && form.querySelector<HTMLFieldSetElement>("fieldset#ulgroup")
 const inputUlEl = form && form.querySelector<HTMLInputElement>("input[name='ul'][value='yes']")
 const ulFields = form && form.querySelectorAll<HTMLInputElement>("input[data-forul]")
+const phoneEl = form && form.querySelector<HTMLInputElement>("input[name='phone']")
+const errorEls = form && form.querySelectorAll<HTMLInputElement>(".excursion-form__error")
+
+const phoneMask =
+    phoneEl &&
+    IMask(phoneEl, {
+        mask: "+{7}(000)000-00-00",
+        lazy: false,
+    })
 
 fieldsetUlEl &&
     fieldsetUlEl.addEventListener("input", (event: Event) => {
@@ -51,10 +61,14 @@ form &&
         formObject = {
             ...formObject,
             agree: document.querySelector<HTMLInputElement>("#agree")?.checked,
+            phone: phoneMask?.unmaskedValue,
         }
 
         try {
             schema.parse(formObject)
+
+            errorEls && errorEls.forEach(e => (e.innerHTML = ""))
+
             const res = await fetch(url, {
                 method: "POST",
                 body: new FormData(form),
@@ -79,7 +93,6 @@ form &&
             console.log(fail)
             const errors = fail.errors.map(({ message, path: [name] }) => ({ path: name, message }))
             errors.forEach(({ message, path }) => {
-                console.log("🚀 ~ message, path :", message, path)
                 const label = (
                     form[path] as HTMLInputElement
                 ).parentElement?.querySelector<HTMLElement>(`label[for="${path}"]`)
