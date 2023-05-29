4b74433d83d2a1284606936d3df5e861bbd4c8b9
voskanvl <voskanvl@gmail.com>
2023-05-29 10:16:07 +0300

    rent animation svg

--- src/rent.ts
+++ src/rent.ts
@@ -180,3 +180,31 @@ window.addEventListener("popstate", () => {
     switchTabs()
 })
 //---
+//--- svg animation ---
+const svgEl = document.querySelector<HTMLOrSVGImageElement>(".rent .banner--right > svg")
+
+if (svgEl) {
+    setInterval(() => {
+        svgEl.dispatchEvent(new CustomEvent("fired"))
+    }, 200)
+
+    const dataLights = svgEl.querySelectorAll<SVGPathElement>("path[data-light]")
+
+    svgEl.addEventListener("fired", () => {
+        dataLights &&
+            dataLights.length &&
+            dataLights.forEach(e => {
+                const dice = Math.random()
+                if (dice > 0.5) {
+                    e.dataset.light === "cloud-canvas"
+                        ? (e.style.fill = "#D4ECFF")
+                        : (e.style.fill = "#fff")
+                } else {
+                    e.dataset.light === "cloud-canvas"
+                        ? (e.style.fill = "#295C9A")
+                        : (e.style.fill = "#000")
+                    e.dataset.light === "tube" && (e.style.fill = "#B2D3EA")
+                }
+            })
+    })
+}
