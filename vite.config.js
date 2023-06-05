128972dd9e89bd05d8b455cc5368be43dea30d92
voskanvl <voskanvl@gmail.com>
2023-06-05 12:09:37 +0300

    vite change output names

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,37 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vite"
 // import vitePugPlugin from "vite-plugin-pug-transformer";
-import vitePugPlugin from "./vite-plugin-pug-edited";
-import { resolve, extname, posix } from "path";
-import { readFileSync, readdirSync } from "fs";
-import * as yaml from "js-yaml";
-import colors from "picocolors";
-import * as fs from "fs";
-import * as path from "path";
-import sassGlobImports from "vite-plugin-sass-glob-import";
+import vitePugPlugin from "./vite-plugin-pug-edited"
+import { resolve, extname, posix } from "path"
+import { readFileSync, readdirSync } from "fs"
+import * as yaml from "js-yaml"
+import colors from "picocolors"
+import * as fs from "fs"
+import * as path from "path"
+import sassGlobImports from "vite-plugin-sass-glob-import"
 
 const merge = () => {
-    console.log(`now merging data files`);
-    const fn = { json: JSON.stringify, yaml: yaml.load, yml: yaml.load };
-    const files = readdirSync(resolve("src/data"));
+    console.log(`now merging data files`)
+    const fn = { json: JSON.stringify, yaml: yaml.load, yml: yaml.load }
+    const files = readdirSync(resolve("src/data"))
     return files.reduce(
         (acc, file) => ({
             ...acc,
             ...fn[extname(file).slice(1)](readFileSync(resolve("src/data", file))),
         }),
         {},
-    );
-};
+    )
+}
 
 function htmlsFiles() {
-    let files = fs.readdirSync(path.resolve(process.cwd()));
+    let files = fs.readdirSync(path.resolve(process.cwd()))
     files = files
         .filter(e => path.extname(e) === ".html")
-        .map(e => path.basename(e).replace(".html", ""));
-    return files;
+        .map(e => path.basename(e).replace(".html", ""))
+    return files
 }
-const htmls = htmlsFiles();
+const htmls = htmlsFiles()
 
-const input = htmls.reduce((acc, e) => ({ ...acc, [e]: resolve(e + ".html") }), {});
+const input = htmls.reduce((acc, e) => ({ ...acc, [e]: resolve(e + ".html") }), {})
 
 export default defineConfig({
     // plugins: [pugPlugin.default(options, locals)],
@@ -42,6 +42,11 @@ export default defineConfig({
                 main: resolve("index.html"),
                 ...input,
             },
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`,
+            },
         },
     },
     resolve: {
@@ -50,4 +55,4 @@ export default defineConfig({
             "@var": resolve("./src/sass/_variables.sass"),
         },
     },
-});
+})
