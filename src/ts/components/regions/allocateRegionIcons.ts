33ed88a87e20457788ced6d17f9b88a61dc5ec76
voskanvl <voskanvl@gmail.com>
2023-06-22 10:14:53 +0300

    change allocateRegionIcons.ts

--- src/ts/components/regions/allocateRegionIcons.ts
+++ src/ts/components/regions/allocateRegionIcons.ts
@@ -1,34 +1,30 @@
 export default function allocateRegionIcons() {
-    const container = document.querySelector<HTMLElement>("#regions > svg");
+    const container = document.querySelector<HTMLElement>("#regions > svg")
     if (!container) {
-        console.warn("there isn't #regions > svg");
-        return;
+        console.warn("there isn't #regions > svg")
+        return
     }
-    const containerRect = container.getBoundingClientRect();
+    const containerRect = container.getBoundingClientRect()
 
-    const iconGroups = [...document.querySelectorAll<HTMLElement>(".geonet__region")];
-    const mapEl = document.querySelector<HTMLElement>("#regions");
+    const iconGroups = [...document.querySelectorAll<HTMLElement>(".geonet__region")]
+
+    const mapEl = document.querySelector<HTMLElement>("#regions")
+    if (!mapEl) throw Error("отсутвтует #map")
+    const { x: mapLeft } = mapEl.getBoundingClientRect()
 
     const pathElements = [...document.querySelectorAll<SVGElement>("#regions > svg path")].filter(
         e => e.getAttribute("regionid"),
-    );
-
-    if (!mapEl) throw Error("отсутвтует #map");
-    const { x: mapLeft } = mapEl.getBoundingClientRect();
+    )
 
     iconGroups.forEach(group => {
-        const groupRegionId = group.dataset.regionid;
-        const path = pathElements.find(path => path.getAttribute("regionid") === groupRegionId);
-        if (!path) throw Error("отсутствует path, соотвествующий regionid=" + groupRegionId);
-        let { x, y } = path.getBoundingClientRect();
+        const groupRegionId = group.dataset.regionid
+        const path = pathElements.find(path => path.getAttribute("regionid") === groupRegionId)
+        if (!path) throw Error("отсутствует path, соотвествующий regionid=" + groupRegionId)
+        let { x, y, width } = path.getBoundingClientRect()
 
-        // group.style.top = y + height / 2 + "px";
-        // group.style.left = x + width / 2 + "px";
-        // x = x + containerRect.x;
-        y -= containerRect.y;
-        // x += containerRect.x;
+        y -= containerRect.y
 
-        group.style.top = y + "px";
-        group.style.left = x - mapLeft + "px";
-    });
+        group.style.top = y + "px"
+        group.style.left = x + width / 2 - mapLeft + "px"
+    })
 }
